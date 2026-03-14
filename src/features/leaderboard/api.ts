8b933881917c734872815b84a24e145f9083ef72
Khalil Ltaief <ltaief.khalil@gmail.com>
2026-03-14 02:17:53 +0100

    fix

--- src/features/leaderboard/api.ts
+++ src/features/leaderboard/api.ts
@@ -9,7 +9,7 @@ export type R6Player = {
 export async function getR6Leaderboard(page: number = 1): Promise<R6Player[]> {
   const targetUrl = `https://api.r6data.eu/api/stats?type=leaderboards&page=${page}`
   const response = await fetch(
-    `https://thingproxy.freeboard.io/fetch/${targetUrl}`,
+    `https://cors-anywhere.herokuapp.com/${targetUrl}`,
     {
       headers: {
         "api-key": import.meta.env.VITE_R6_API_KEY as string,
